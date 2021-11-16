1263018aa1b492573af258ec2a910fc2bbbdc12b
booding <booding.xiu@outlook.com>
2021-11-16 16:27:18 +0800

    edit the API config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,2 @@
-export let API: string = "http://localhost:3000/users"
+export let API: string = "http://localhost:2999/"
 
-// if (process.env.NODE_ENV === "development") {
-//     API = process.env.REACT_APP_DEVELOPMENT_API_URL!
-// } else if (process.env.NODE_ENV === "production") {
-//     API = process.env.REACT_APP_PRODUCTION_API_URL!
-// }
