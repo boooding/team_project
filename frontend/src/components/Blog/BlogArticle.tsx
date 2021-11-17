11f31d19a8834d2cd4a29d94197194af84f39de7
booding <booding.xiu@outlook.com>
2021-11-17 14:07:26 +0800

    update

--- frontend/src/components/Blog/BlogArticle.tsx
+++ frontend/src/components/Blog/BlogArticle.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import "./BlogArticle.css"
+import MarkdownEditor from '@uiw/react-markdown-editor'
 
 const BlogArticle  = () => {
     return <div className="module-box">
         <div className="module-title divCenter">
             Article
         </div>
-
+        <MarkdownEditor
+        />
     </div>
 }
 export default BlogArticle;
--- frontend/src/components/Blog/BlogInfo.tsx
+++ frontend/src/components/Blog/BlogInfo.tsx
@@ -12,7 +12,6 @@ const BlogInfo  = () => {
             />
         </div>
 
-
         <div className="info-username textCenter">
             <div>
                 Booding
