bd4b93b893459c930c800b252f8a11a6a6099007
booding <booding.xiu@outlook.com>
2021-11-29 23:57:01 +0800

    edit article components

--- frontend/src/components/Article/Article.tsx
+++ frontend/src/components/Article/Article.tsx
@@ -1,11 +1,48 @@
 import React from "react"
 import LayoutForArticle from "./LayoutForArticle";
+import {Col, Menu, Row} from "antd";
+import Layout from "../core/Layout";
+import MenuItem from "antd/es/menu/MenuItem";
+import {Link} from "react-router-dom";
+import {UnorderedListOutlined, BlockOutlined} from '@ant-design/icons';
+import { Typography } from 'antd';
 
 const Article  = () => {
-    return <LayoutForArticle>
-        <div>
-            Hello, Article
+    const articleLinks = () => {
+        const {Title} = Typography;
+        return  <React.Fragment>
+            <Title level={5}>Article links</Title>
+            <Menu>
+                <MenuItem>
+                    <Link to="/article">
+                        <UnorderedListOutlined />Article List</Link>
+                </MenuItem>
+                <MenuItem>
+                    <Link to="/article">
+                        <BlockOutlined />New Article</Link>
+                </MenuItem>
+            </Menu>
+        </React.Fragment>
+    }
+
+    const articleList = () => {
+        return <div>
+            articleList
         </div>
-    </LayoutForArticle>
+    }
+    return <Layout title="" subTitle="">
+        <div style={{
+            marginTop:"30px"
+        }}>
+            <Row gutter={32}>
+                <Col span="5">
+                    {articleLinks()}
+                </Col>
+                <Col span="19">
+                    {articleList()}
+                </Col>
+            </Row>
+        </div>
+    </Layout>
 }
 export default Article;
