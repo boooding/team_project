5044ac29f2b2df5afaeee427d6b3e05b608adf5f
booding <booding.xiu@outlook.com>
2021-12-12 17:57:28 +0800

    update

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -4,7 +4,7 @@ import {createHashHistory} from "history";
 import {routerMiddleware} from "connected-react-router";
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from "./sagas";
-// import the development tools
+
 import {composeWithDevTools} from "redux-devtools-extension";
 
 export const history = createHashHistory()
