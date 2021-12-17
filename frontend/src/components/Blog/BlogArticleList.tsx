9f0d6225c0e9bd5510b6c543f19c7a6c020ec995
booding <booding.xiu@outlook.com>
2021-12-17 20:40:53 +0800

    update

--- frontend/src/components/Blog/BlogArticleList.tsx
+++ frontend/src/components/Blog/BlogArticleList.tsx
@@ -1,12 +1,5 @@
 import React, {useEffect, useState} from "react"
 import "./BlogArticle.css"
-import {isAuth} from "../../commonFunction/auth";
-import {UserJwt} from "../../store/models/authority";
-import axios from "axios";
-import {API} from "../../config";
-import {CalendarOutlined, UserOutlined} from "@ant-design/icons";
-import {Button, Divider, Modal} from "antd";
-
 import 'markdown-navbar/dist/navbar.css';
 
 // @ts-ignore
@@ -14,6 +7,10 @@ import marked from 'marked';
 import hljs from "highlight.js";
 import 'highlight.js/styles/monokai-sublime.css';
 import BlogArticleListContent from "./BlogArticleListContent";
+import {isAuth} from "../../commonFunction/auth";
+import {UserJwt} from "../../store/models/authority";
+import axios from "axios";
+import {API} from "../../config";
 
 const renderer = new marked.Renderer();
 marked.setOptions({
@@ -34,36 +31,23 @@ interface IArticleList {
     ArticleList: any
 }
 
-const BlogArticleList: React.FC<IArticleList> = ({ArticleList}) => {
-    let data = [ "61a714ebc6f43d0ba0d8e948","61a58944796f578c31d68358",]
+const BlogArticleList: React.FC<IArticleList> = () => {
+    const [data, setData ] = useState()
 
-    // @ts-ignore
-    // const { user, token } = isAuth() as UserJwt;
-    // useEffect(() => {
-    //     getArticle();
-    // }, []);
-    //
-    // async function getArticle() {
-    //     try {
-    //         let response = await axios.get(`${API}/blogs/${user._id}`)
-    //         // @ts-ignore
-    //         setArticleList(response)
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // }
+    const { user, token } = isAuth() as UserJwt;
+
+    async function getArticleList() {
+        try {
+            let response = await axios.get(`${API}/blogs/${user._id}`)
+            // @ts-ignore
+            setArticleList(response)
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
     return <>
-        {data.map((value, index) => {
-            return <BlogArticleListContent key={value + index} value={value}/>
-        }
-        )}
+        <div></div>
     </>
-
-
-
-
-
-
 }
 export default BlogArticleList;
